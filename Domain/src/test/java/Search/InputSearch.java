2c5c9056d7cbf14fc9a467830bdd812f1c505b6b
ismayatikiran <ismayatikiran96@gmail.com>
2018-09-16 09:44:25 -0400

    SeleniumTest1

---
+++ Domain/src/test/java/Search/InputSearch.java
@@ -0,0 +1,12 @@
+package Search;
+
+import base.CommonAPI;
+
+public class InputSearch extends CommonAPI {
+    public void search(){
+        //driver.findElement()
+    }
+
+
+
+}
--- Generic/src/main/java/base/CommonAPI.java
+++ Generic/src/main/java/base/CommonAPI.java
@@ -1,4 +1,26 @@
 package base;
 
+import com.beust.jcommander.Parameter;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class CommonAPI {
+    public WebDriver driver = null;
+
+    @BeforeMethod
+    public void setUp(){
+        //System.setProperty();
+        driver = new FirefoxDriver();
+       // driver.manage().timeouts().implicitlyWait();
+
+ }
+
+    @AfterMethod
+
+    public void cleanUp(){
+        driver.close();
+    }
+
 }
